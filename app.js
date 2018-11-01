b9162496370f2c09965d7720c7e0bf18851e6616
seymakara <seymakara88@gmail.com>
2018-11-01 16:24:12 -0700

    edit and delete are partially working :)

--- app.js
+++ app.js
@@ -2,19 +2,17 @@ $(document).ready(function(){
   
   
   var refreshWatchTable = function(){
-    $("#toWatchTable").empty();
+    $("#toWatchTable td").empty();
     for (var i = 0; i < localStorage.length; i++){
       var movie = JSON.parse(localStorage.getItem(localStorage.key(i)));
-      console.log("movie", movie)
-      $("#toWatchTable").append(`<tr align = 'middle' ><td>${movie[0]}</td><td>${movie[1]}</td><td>${movie[2]}</td><td>${movie[3]}</td><td><button>Yes</button></td><td><button>Edit</button><button class = 'deleteButton' id ='${movie[0]}'>Delete</button></td></tr>`);
+      // console.log("movie", movie)
+      $("#toWatchTable").append(`<tr align = 'middle' ><td>${movie[0]}</td><td>${movie[1]}</td><td>${movie[2]}</td><td>${movie[3]}</td><td><button class = 'yesButton'>Yes</button></td><td><button class = 'editButton' id ='${movie[0]}'>Edit</button><button class = 'deleteButton' id ='${movie[0]}'>Delete</button></td></tr>`);
     }
   }
+
+
   
   refreshWatchTable();
-
-
-
-  
 
   var movieDetails = []
 
@@ -22,6 +20,7 @@ $(document).ready(function(){
   $(".add-text-btn").on("click", function(){
     // $(".show-text").empty();
 
+    
     var curTitleValue = $('#title').val(); // reading from <input>
     var curKeyValue = curTitleValue; // change to dynamic key?
     var curGenreValue = $('#genre').val();
@@ -29,8 +28,6 @@ $(document).ready(function(){
     var curScoreValue = $('#score').val();
 
     movieDetails = localStorage.getItem(curTitleValue) ? JSON.parse(localStorage.getItem(curTitleValue)) : [];
-
-    // localStorage.setItem(curKeyValue, JSON.stringify(movieDetails));
    
 
     movieDetails.push(curTitleValue)
@@ -38,39 +35,78 @@ $(document).ready(function(){
     movieDetails.push(curYearValue)
     movieDetails.push(curScoreValue)
 
-    console.log("movieDetails", movieDetails)
+    // console.log("movieDetails", movieDetails)
     
     localStorage.setItem(curKeyValue, JSON.stringify(movieDetails));
-    // var data = JSON.parse(localStorage.getItem(curKeyValue));
-    // console.log("data", data[0])
     
-
     movieDetails = []
-
-    // var createList = function(movie){
-    //   $("#toWatchTable").append(`<tr align = 'middle' ><td>${movie[0]}</td><td>${movie[1]}</td><td>${movie[2]}</td><td>${movie[3]}</td><td><button>Yes</button></td><td><button>Edit</button><button>Delete</button></td></tr>`);
-    // }
     refreshWatchTable();
-
-    // createList(data);
-
-    // var storage = window.localStorage
-    // for(item in storage){
-    //   console.log("item", item)
-    //   console.log("item", item[curKeyValue])
-
-    //   localStorage.setItem(curKeyValue, JSON.stringify(movieDetails));
-    //   var data = JSON.parse(localStorage.getItem(item[curKeyValue]));
-    //   createList(data);
-    // }
 
     
   });
 
   $(document).on('click',".deleteButton", function(){
     console.log(event)
+    console.log("eventsrc", localStorage[event.srcElement.id])
+    console.log("editkeyValue1", editKeyValue)
     localStorage.removeItem(event.srcElement.id);
+
     refreshWatchTable();
+
+  });
+
+
+  var valueToEdit;
+  var editArray = []
+  var editKeyValue;
+
+
+  $(document).on('click',".update-movie", function(){
+
+    // console.log("before ", localStorage[valueToEdit])
+    
+    
+    var editTitleValue = $('#title').val(); // reading from <input>
+    // editKeyValue = editTitleValue; // change to dynamic key?
+    // console.log("editKeyValue2", editKeyValue)
+    var editGenreValue = $('#genre').val();
+    var editYearValue = $('#year').val();
+    var editScoreValue = $('#score').val();
+
+    editArray.push(editTitleValue)
+    editArray.push(editGenreValue)
+    editArray.push(editYearValue)
+    editArray.push(editScoreValue)
+    
+    // console.log("editArray ", editArray)
+
+    localStorage.setItem(valueToEdit, JSON.stringify(editArray))
+    
+
+    editArray = []
+
+    // console.log("after ", localStorage[valueToEdit])
+
+    refreshWatchTable();
+
+  });
+
+  $(document).on('click',".editButton", function(){
+    // console.log(event)
+    var itemToEdit = event.srcElement.id
+    // console.log("itemToedit ", itemToEdit)
+    var movieToEdit = JSON.parse(localStorage.getItem(itemToEdit));
+    // console.log("movieToEdit ", movieToEdit)
+
+    valueToEdit = (movieToEdit[0])
+    console.log("valueToEdit", valueToEdit)
+
+    $('#title').val(movieToEdit[0])
+    $('#genre').val(movieToEdit[1]) 
+    $('#year').val(movieToEdit[2]) 
+    $('#score').val(movieToEdit[3])
+
+    // refreshWatchTable();
 
   });
 
@@ -79,13 +115,10 @@ $(document).ready(function(){
 
 
 
-  // remove item from app
 
-  // listen for click event (del)
   $(".clear-cache-btn").on("click", function(){
     // clear local storage
     localStorage.clear();
-    // $(".show-text").empty();
   });
 
 });
